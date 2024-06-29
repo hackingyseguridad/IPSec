386952c57e34f09c83c58b0ae3d5dca295ecfed1
hackingyseguridad <antonio_taboada@hotmail.es>
2024-06-29 23:28:32 +0200

    Update ivanti.sh

--- ivanti.sh
+++ ivanti.sh
@@ -1,3 +1,3 @@
 # cliente consola para acceso VPN IVANTI
 #
-openconnect --protocol=pulse --useragent "Pulse-Secure/22.2.1.1295" --os=win  -vvv --user==usuario  https://vpn.hackingyseguriad.com
+openconnect --protocol=pulse --useragent "Pulse-Secure/22.2.1.1295" --os=win  -vvv  --dump-http-traffic --user==usuario  https://vpn.hackingyseguriad.com
